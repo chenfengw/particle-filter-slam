ee37747f83fc7778bf2afd44460d1fe204c69042
Charlie <457511811@qq.com>
2021-02-23 22:07:18 -0800

    add to readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# particle-filter-slam
+# Particle Filter SLAM
+
+## Requirements
+
+To install requirements:
+
+```setup
+pip install -r requirements.txt
+```
+
+## SLAM
+
+To perform SLAM, run this script:
+
+```train
+slam_main.py
+```
+> It's recommended to run this script in Jupyter environment.
+
+## Visualization
+To visualize mapping and localization process, run:
+```eval
+plot_map.py
+```
+> It's recommended to run this script in Jupyter environment.
--- slam_main.py
+++ slam_main.py
@@ -2,20 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-import importlib
 from skimage.draw import line
 from tqdm import tqdm_notebook
-import sensors
-import occupancy_map
-import particle_filter
-import transform
 import cv2
-
-importlib.reload(sensors)
-importlib.reload(occupancy_map)
-importlib.reload(particle_filter)
-importlib.reload(transform)
-
+import sensors
 from occupancy_map import Map
 from transform import Transform
 from particle_filter import ParticleFilter
@@ -29,8 +19,8 @@ myMap = Map(res=1,x_range=[-1300,1300],y_range=[-1200,1200])
 tf = Transform()
 
 # %% Main Loop
-pf = ParticleFilter(n_particles=1, add_noise=False)
-gyro_range = int(gyro.get_length() * 0.01)
+pf = ParticleFilter(n_particles=10, add_noise=True)
+gyro_range = int(gyro.get_length() * 1)
 now = time.time()
 car_trajactory = np.zeros([2,gyro_range])
 
@@ -39,8 +29,6 @@ encoder_idx = 0
 update_count = 0
 max_idx = min(encoder.get_length()-1, lidar.get_length()-1)
 update_now = False
-map_progress = []
-traj_progress = []
 
 for gyro_idx in tqdm_notebook(range(gyro_range)):
     t_loop = time.time()
@@ -66,21 +54,21 @@ for gyro_idx in tqdm_notebook(range(gyro_range)):
         lidar_scan = lidar.polar_to_xy(encoder_idx)
 
         # update alpha, find max particle
-        # max_particle, max_correlation = pf.update(lidar_scan, myMap, tf)
-        max_particle = pf.particles[:,0]
+        max_particle, max_correlation = pf.update(lidar_scan, myMap, tf)
+        # max_particle = pf.particles[:,0]
         car_trajactory[:,update_count] = max_particle[:2]
 
-        # use max alpha to update update map
+        # use max particle to update update map
         lidar_world = tf.lidar_to_world(lidar_scan, max_particle)
         x_cells, y_cells = myMap.meter_to_cell(lidar_world[:2,:])
 
+        # find cells that ray pass through
         scaned_cell = []
         for x_cell1, y_cell1 in zip(x_cells,y_cells):
             particle_x = np.round(pf.particles[0,0] / myMap.res).astype(int)
             particle_y = np.round(pf.particles[1,0] / myMap.res).astype(int)
             x_cell, y_cell = line(particle_x,particle_y,x_cell1,y_cell1)
             scaned_cell.append((x_cell,y_cell))
-
         myMap.update_log_odds(scaned_cell)
         update_now = False
 
@@ -93,7 +81,7 @@ print("total: {} loops, took {} min".format(gyro_idx, (time.time()- now)/60))
 origin = np.zeros([2,1])
 car_trajactory = np.hstack((origin, car_trajactory))
 
-# %%
+# %% plot car trajectory
 plt.scatter(car_trajactory[0][::100],car_trajactory[1][::100])
 # %%
 plt.figure()
